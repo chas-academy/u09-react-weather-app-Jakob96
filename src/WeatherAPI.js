3919bc76a5b26ddd8de7af8dcc155fe3a35ebc00
Jakob96 <jakob96@outlook.com>
2021-04-22 19:43:17 +0200

    Implements method for fetching weather data

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -7,4 +7,15 @@ export default class WeatherAPI {
         console.error(error);
     })
  }
+
+ static async getWeatherData(lat, lon, units) {
+    const query = `lat=${lat}&lon=${lon}`
+
+    return await fetch(`${process.env.REACT_APP_API_URL}/data/2.5/onecall?${query}&units=${units}&APPID=${process.env.REACT_APP_API_ID}&lang=${(navigator.language) ? navigator.language.substr(0, 2) : 'en'}`)
+    .then(response => response.json())
+    .catch(error => {
+        console.error(error);
+    });
+ };
+
 }
