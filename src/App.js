427d8179b1cab167a74bca4b79f3da904eec98ce
Jakob96 <jakob96@outlook.com>
2021-04-24 13:07:26 +0200

    Implements useEffects for fetching current position and fetching location and weather data and save to states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.scss';
+import WeatherAPI from './WeatherAPI';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [units, setUnits] = useState('metric');
+  const [location, setLocation] = useState('');
+  const [temp, setTemp] = useState();
+  const [weatherDesc, setWeatherDesc] = useState('');
+  const [coordinates, setCoordinates] = useState([]);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => { setCoordinates([pos.coords.latitude, pos.coords.longitude]); }, (err) => { console.error(err) });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (coordinates.length) {
+      WeatherAPI.getLocation(coordinates[0], coordinates[1]).then(response => {
+        setLocation(response[0].local_names.feature_name);
+      });
+
+      WeatherAPI.getWeatherData(coordinates[0], coordinates[1], units).then(response => {
+        setTemp(response.current.temp);
+        setWeatherDesc(response.current.weather[0].description);
+      });
+  }
+  }, [coordinates, setCoordinates, units, setUnits])
+
   return (
-   <div></div>
+    <main>
+      
+    </main>
   );
 }
 
